3dcc51bc94fb76c01bf17f89671cfb791d4c5728
Rahultota <rahultota2001@gmail.com>
2024-09-07 10:55:11 +0530

    add color

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './WelcomeScreen.css'
 const WelcomeScreen = ({ startSurvey }) => {
   return (
     <div className="welcome-screen">
       <h1>Welcome to Our Customer Survey</h1>
-      <button onClick={startSurvey}>Start Survey</button>
+      <button onClick={startSurvey}  className="red">Start Survey</button>
     </div>
   );
 };
